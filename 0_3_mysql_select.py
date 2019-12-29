091dfb251ad2b1ee01cf8451b2cf083c57ce8f5d
zlqlpp <zlqlpp@gmail.com>
2019-12-29 20:29:39 +0800

    code

--- 0_3_mysql_select.py
+++ 0_3_mysql_select.py
@@ -16,3 +16,44 @@ except Exception as err:
  
 
 db.close()
+
+####
+sql = "SELECT * FROM EMPLOYEE \
+       WHERE INCOME > %s" % (1000)
+try:
+   cursor.execute(sql)
+   results = cursor.fetchall()
+   for row in results:
+      fname = row[0]
+      lname = row[1]
+      age = row[2]
+      sex = row[3]
+      income = row[4]
+      print "fname=%s,lname=%s,age=%s,sex=%s,income=%s" % \
+             (fname, lname, age, sex, income )
+except:
+   print "Error: unable to fecth data"
+
+db.close()
+
+
+# SQL 更新语句
+sql = "UPDATE EMPLOYEE SET AGE = AGE + 1 WHERE SEX = '%c'" % ('M')
+try:
+   cursor.execute(sql)
+   db.commit()
+except:
+   db.rollback()
+
+db.close()
+
+
+# SQL 删除语句
+sql = "DELETE FROM EMPLOYEE WHERE AGE > %s" % (20)
+try:
+   cursor.execute(sql)
+   db.commit()
+except:
+   db.rollback()
+
+db.close()
