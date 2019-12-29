b47353492eb11f12b620789af0e6ac6609a8935f
zlqlpp <zlqlpp@gmail.com>
2019-12-29 20:49:24 +0800

    code

---
+++ 3_1_lianxifangshi_id.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import pymysql
+import time
+import logging
+import re
+
+logging.basicConfig(filename='lianxifangshi.log',level=logging.DEBUG)
+
+def getStr(title):
+    p = re.compile('[0-9a-zA-Z]')
+    res = p.findall(title)
+    str = ''
+    for i in res:
+      str += i
+    #print(str)
+    return str
+	
+db = pymysql.connect("127.0.0.1","root","qrkcgya520","yixian" )
+cursor = db.cursor()
+
+#sql = "SELECT id,title FROM qqvx_1  WHERE INCOME > %s" % (1000)
+sql = "SELECT id,title FROM qqvx_1  where title_id is null limit 100 " 
+ 
+try:
+   cursor.execute(sql)
+   results = cursor.fetchall()
+   for row in results:
+      id = row[0]
+      title = row[1]
+      
+	  cursor.execute("UPDATE qqvx_1 SET title_id='%s' WHERE id = '%d'" % (getStr(title),id))
+
+      
+except:
+   print "Error: unable to fecth data"
+
+db.close()
+
+ 
