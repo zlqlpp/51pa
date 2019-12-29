e9e9242010e48c737e757c835898ecdeec87146d
zlqlpp <zlqlpp@gmail.com>
2019-12-29 21:17:53 +0800

    code

--- 3_1_lianxifangshi_id.py
+++ 3_1_lianxifangshi_id.py
@@ -20,9 +20,9 @@ db = pymysql.connect("127.0.0.1","root","qrkcgya520","yixian" )
 cursor = db.cursor()
 
  
- 
+
 try:
-   cursor.execute("SELECT id,title FROM qqvx_1  where title_id is null limit 100 ")
+   cursor.execute("SELECT id,title FROM qqvx_1  where title_id is null limit 1000 ")
    results = cursor.fetchall()
    for row in results:
       id = row[0]
