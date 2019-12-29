d7b53d8082a858f6498d111264b78161f7fe60be
zlqlpp <zlqlpp@gmail.com>
2019-12-29 21:01:33 +0800

    code

--- 3_1_lianxifangshi_id.py
+++ 3_1_lianxifangshi_id.py
@@ -15,21 +15,20 @@ def getStr(title):
       str += i
     #print(str)
     return str
-	
+    
 db = pymysql.connect("127.0.0.1","root","qrkcgya520","yixian" )
 cursor = db.cursor()
 
-#sql = "SELECT id,title FROM qqvx_1  WHERE INCOME > %s" % (1000)
-sql = "SELECT id,title FROM qqvx_1  where title_id is null limit 100 " 
+ 
  
 try:
-   cursor.execute(sql)
+   cursor.execute("SELECT id,title FROM qqvx_1  where title_id is null limit 100 ")
    results = cursor.fetchall()
    for row in results:
       id = row[0]
       title = row[1]
       
-	  cursor.execute("UPDATE qqvx_1 SET title_id='%s' WHERE id = '%d'" % (getStr(title),id))
+      cursor.execute("UPDATE qqvx_1 SET title_id='%s' WHERE id = '%d'" % (getStr(title),id))
 
       
 except:
