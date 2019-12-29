e56f1d3724aee82593915c04518d605a1439d9e5
zlqlpp <zlqlpp@gmail.com>
2019-12-29 20:16:19 +0800

    code

--- 0_3_mysql_insert.py
+++ 0_3_mysql_insert.py
@@ -1,18 +1,30 @@
-import pymysql
- 
+import MySQLdb
 
-db = pymysql.connect("127.0.0.1","root","qrkcgya520","yixian" )
+# 打开数据库连接
+db = MySQLdb.connect("localhost", "testuser", "test123", "TESTDB", charset='utf8' )
+
+# 使用cursor()方法获取操作游标 
 cursor = db.cursor()
- 
 
-sql = """INSERT INTO qqvx( num ,url, title, date, realdate, content)
-         VALUES (1, 'Mohan', 'Mohan', 'M', 'Mohan','ss')"""
+# SQL 查询语句
+sql = "SELECT * FROM EMPLOYEE \
+       WHERE INCOME > %s" % (1000)
 try:
+   # 执行SQL语句
    cursor.execute(sql)
-   db.commit()
-except Exception as err:
-   print(err)
-   db.rollback()
- 
+   # 获取所有记录列表
+   results = cursor.fetchall()
+   for row in results:
+      fname = row[0]
+      lname = row[1]
+      age = row[2]
+      sex = row[3]
+      income = row[4]
+      # 打印结果
+      print "fname=%s,lname=%s,age=%s,sex=%s,income=%s" % \
+             (fname, lname, age, sex, income )
+except:
+   print "Error: unable to fecth data"
 
+# 关闭数据库连接
 db.close()
