8db7569c2b99f8106d1100a95183857850cf3d68
zlqlpp <zlqlpp@gmail.com>
2020-01-06 17:20:04 +0800

    \

--- 3_1_lianxifangshi_id.py
+++ 3_1_lianxifangshi_id.py
@@ -8,8 +8,17 @@ import re
 
 logging.basicConfig(filename='get_id_in_tile.log',level=logging.DEBUG)
 
-def getStr(title):
+def getidFromStr(title):
     p = re.compile('[0-9a-zA-Z]')
+    res = p.findall(title)
+    str = ''
+    for i in res:
+      str += i
+    #print(str)
+    return str
+    
+def getcnFromStr(title):
+    p = re.compile('[\u4e00-\u9fa5]+')
     res = p.findall(title)
     str = ''
     for i in res:
@@ -23,14 +32,14 @@ cursor = db.cursor()
  
 
 try:
-   cursor.execute("SELECT id,title FROM qqvx_1  where title_id is null limit 1000 ")
+   cursor.execute("SELECT id,title FROM qqvx_1  where title_id is null limit 10000 ")
    results = cursor.fetchall()
    for row in results:
       id = row[0]
       title = row[1]
       #print(id, title )
       #print(getStr(title))
-      su = "UPDATE qqvx_1 SET title_id='%s' WHERE id = %d" %  (getStr(title),id)
+      su = "UPDATE qqvx_1 SET title_id='%s',title_cn='%s' WHERE id = %d" %  (getidFromStr(title),getcnFromStr(title),id)
       cursor.execute(su)
       db.commit()
 
