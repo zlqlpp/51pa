b3e84a597d904ff6d1f05af844093fae7a961ccc
zlqlpp <zlqlpp@gmail.com>
2020-01-06 17:14:29 +0800

    \

--- 0_4_regex.py
+++ 0_4_regex.py
@@ -2,7 +2,8 @@
 import re
 
 p = re.compile('[0-9a-zA-Z]')
-res = p.findall('b315鑫鑫联系方式购买帖!')
+p2 = re.compile('[\u4e00-\u9fa5]+')
+res = p2.findall('b315鑫鑫联系方式购买帖!')
 str = ''
 for i in res:
  str += i
